3901de48c1906c13a13f307988e816c4d8795f89
robertsteele7 <atpcycle7@gmail.com>
2022-10-23 13:18:00 -0500

    completed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace LINQ
 {
+    //Create a list of video game names..
+    //Order the list of games by length of the game name.
+    //Use the lambda expression in this exercise as well
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Games = new List<string>
+            {"PACMAN","SPACEINVADERS","MARIO","QBERT","W.O.W.","TETRIS"
+
+            };
+            IEnumerable<String> Order = Games.OrderBy(x => x.Length);
+            foreach (string Game in Order)
+            {
+                Console.WriteLine(Game);
+            }
+            Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
         }
     }
 }
